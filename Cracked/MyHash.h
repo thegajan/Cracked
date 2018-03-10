ca2fff04a5c05d6b819631d154fcc85e74fc77db
thegajan <thegajannagaraj@gmail.com>
2018-03-10 15:23:42 -0800

    finished myhash

--- Cracked/MyHash.h
+++ Cracked/MyHash.h
@@ -28,4 +28,139 @@ public:
     MyHash& operator=(const MyHash&) = delete;
 
 private:
+	struct Node {
+		Node(KeyType key, ValueType value) {
+			m_key = key;
+			m_value = value;
+		}
+		KeyType m_key;
+		ValueType m_value;
+		Node* m_next = nullptr;
+	};
+
+
+	void newHashTable();
+	void deleteHash();
+	void insert(Node* hashTable, Node* newNode);
+
+	Node* m_hash = nullptr;
+	double m_maxLoadFactor;
+	int m_size;
+	int m_numHash = 0;
 };
+
+//implementaitons of myhash member funcitons
+
+//MyHash Constructor
+template<typename KeyType, typename ValueType>
+MyHash<KeyType, ValueType>::MyHash(double maxLoadFactor = 0.5) {
+	m_size = 100;
+	m_hash = new Node*[m_size];
+	if (maxLoadFactor <= 0)
+		m_maxLoadFactor = 0.5;
+	else if (maxLoadFactor > 2.0)
+		m_maxLoadFactor = 2.0;
+	else
+		m_maxLoadFactor = maxLoadFactor;
+}
+
+//MyHash destructor
+template<typename KeyType, typename ValueType>
+MyHash<KeyType, ValueType>::~MyHash() {
+	deleteHash();
+}
+
+//MyHash reset function
+template<typename KeyType, typename ValueType>
+void MyHash<KeyType, ValueType>::reset() {
+	deleteHash();
+	m_hash = new Node*[m_size];
+}
+
+//MyHash associate function
+template<typename KeyType, typename ValueType>
+void MyHash<KeyType, ValueType>::associate(const KeyType& key, const ValueType& value) {
+	ValueType* v = find(key);
+	if (v == nullptr) {
+		m_numHash++;
+		if ((m_numHash) / m_size > m_maxLoadFactor)
+			newHashTable();
+		Node* p = new Node(key, value);
+		insert(m_hash, p);
+	}
+	else {
+		*v = value;
+	}
+}
+
+//delete the hash table
+template<typename KeyType, typename ValueType>
+void MyHash<KeyType, ValueType>::deleteHash() {
+	for (int i = 0; i < m_size; i++) {
+		Node* p = m_hash[i];
+		while (p != nullptr)
+		{
+			Node* k = p;
+			p = p->next();
+			delete k;
+		}
+	}
+	delete m_hash;
+}
+
+//MyHash getNumItems
+template<typename KeyType, typename ValueType>
+int MyHash<KeyType, ValueType>::getNumItems() const {
+	return m_numHash;
+}
+
+//MyHash getLoadFactor
+template<typename KeyType, typename ValueType>
+double MyHash<KeyType, ValueType>::getLoadFactor() const {
+	return m_maxLoadFactor;
+}
+
+//MyHash find
+template<typename KeyType, typename ValueType>
+const ValueType* MyHash<KeyType, ValueType>::find(const KeyType& key) const {
+	int hashSpot = hash(key) % m_size;
+	Node* p = m_hash[hashSpot];
+	if (p->key == key)
+		return *(p->value);
+	else
+		while (p != nullptr) {
+			if (p->key == key)
+				return *(p->value);
+			p = p->next();
+		}
+	return nullptr;
+}
+
+//MyHash newHashTable
+template<typename KeyType, typename ValueType>
+void MyHash<KeyType, ValueType>::newHashTable() {
+	m_size = m_size * 2;
+	Node* newHash = new Node*[m_size];
+	for (int i = 0; i < m_size/2; i++) {
+		Node* p = m_hash[i];
+		while (p != nullptr) {
+			insert(newHash, p);
+			p = p->next;
+		}
+	}
+	deleteHash();
+	m_hash = newHash;
+}
+
+//MyHash insert
+template<typename KeyType, typename ValueType>
+void MyHash<KeyType, ValueType>::insert(Node* hashTable, Node* newNode) {
+	int hashSpot = hash(newNode->key) % m_size;
+	if (hashTable[hashSpot] == nullptr)
+		hashTable[hashSpot] = newNode;
+	else {
+		Node* c = hashTable[hashSpot];
+		newNode->next = c;
+		hashTable[hashSpot] = newNode;
+	}
+}
--- Cracked/WordList.cpp
+++ Cracked/WordList.cpp
@@ -6,42 +6,42 @@ using namespace std;
 class WordListImpl
 {
 public:
-    bool loadWordList(string filename);
-    bool contains(string word) const;
-    vector<string> findCandidates(string cipherWord, string currTranslation) const;
+	bool loadWordList(string filename);
+	bool contains(string word) const;
+	vector<string> findCandidates(string cipherWord, string currTranslation) const;
 private:
 };
 
 bool WordListImpl::loadWordList(string filename)
 {
-    return false;  // This compiles, but may not be correct
+	return false;  // This compiles, but may not be correct
 }
 
 bool WordListImpl::contains(string word) const
 {
-    return false; // This compiles, but may not be correct
+	return false; // This compiles, but may not be correct
 }
 
 vector<string> WordListImpl::findCandidates(string cipherWord, string currTranslation) const
 {
-    return vector<string>();  // This compiles, but may not be correct
+	return vector<string>();  // This compiles, but may not be correct
 }
 
 //***** hash functions for string, int, and char *****
 
 unsigned int hash(const std::string& s)
 {
-    return std::hash<std::string>()(s);
+	return std::hash<std::string>()(s);
 }
 
 unsigned int hash(const int& i)
 {
-    return std::hash<int>()(i);
+	return std::hash<int>()(i);
 }
 
 unsigned int hash(const char& c)
 {
-    return std::hash<char>()(c);
+	return std::hash<char>()(c);
 }
 
 //******************** WordList functions ************************************
@@ -51,25 +51,40 @@ unsigned int hash(const char& c)
 
 WordList::WordList()
 {
-    m_impl = new WordListImpl;
+	m_impl = new WordListImpl;
 }
 
 WordList::~WordList()
 {
-    delete m_impl;
+	delete m_impl;
 }
 
 bool WordList::loadWordList(string filename)
 {
-    return m_impl->loadWordList(filename);
+	return m_impl->loadWordList(filename);
 }
 
 bool WordList::contains(string word) const
 {
-    return m_impl->contains(word);
+	return m_impl->contains(word);
 }
 
 vector<string> WordList::findCandidates(string cipherWord, string currTranslation) const
 {
-   return m_impl->findCandidates(cipherWord, currTranslation);
+	return m_impl->findCandidates(cipherWord, currTranslation);
+}
+
+unsigned int hash(const std::string& s)
+{
+	return
+		std::hash<std::string>()(s);
+}
+unsigned int hash(const int& i)
+{
+	return std::hash<int>()(i);
+}
+
+unsigned int hash(const	char& c)
+{
+	return std::hash<char>()(c);
 }
